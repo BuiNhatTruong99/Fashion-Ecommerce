c699cf5863c303aae406dbcfa5c49ea528418e1c
Truong Bui <bntruong99@gmail.com>
2024-05-30 21:30:59 +0700

    add login page

--- client/src/app/list/page.tsx
+++ client/src/app/list/page.tsx
@@ -2,7 +2,7 @@ import Filter from '@/components/Filter';
 import ProductList from '@/components/ProductList';
 import Image from 'next/image';
 
-const Page = () => {
+const SinglePage = () => {
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       <div className="hidden bg-pink-50 px-4 sm:flex justify-between h-64">
@@ -24,4 +24,4 @@ const Page = () => {
   );
 };
 
-export default Page;
+export default SinglePage;
--- client/src/components/NavIcons.tsx
+++ client/src/components/NavIcons.tsx
@@ -32,7 +32,7 @@ const NavIcons = () => {
       />
       {isProfileOpen && (
         <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20 bg-white">
-          <Link href="/">Profile</Link>
+          <Link href="/login">Profile</Link>
           <div className="mt-2 cursor-pointer">Logout</div>
         </div>
       )}
